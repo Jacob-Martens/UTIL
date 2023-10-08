d3bcc005310f994448d891c017086ba586c125de
Jacob-Martens <jacobmartens@gmail.com>
2023-10-08 12:31:28 -0400

    Created UTIL Semaphore class

---
+++ Semaphore/inc/UTIL_Semaphore.h
@@ -0,0 +1,41 @@
+#pragma once
+/*
+* FILE			: UTIL_Semaphore.h
+* CREATED		: 08/10/2023
+* AUTHOR		: Jacob Martens (jacobmartens@gmail.com)
+* DESCRIPTION	: 
+* Semaphore implementation for ensuring thread-safety of resources
+*/
+#include <mutex>
+#include <condition_variable>
+
+class UTIL_Semaphore {
+private:
+	std::mutex m_mutex;
+	std::condition_variable m_cv;
+	int m_count;
+	int m_max_count;
+
+	const int MIN_COUNT = 0;
+	const int MAX_COUNT_INFINITE = -1;
+
+public:
+	// Creates a UTIL_Semaphore with count of 0 and max_count of infinity
+	UTIL_Semaphore();
+	// Creates a UTIL_Semaphore with provided count and max_count of infinity
+	UTIL_Semaphore(int count);
+	// Creates a UTIL_Semaphore with provided count and max_count
+	UTIL_Semaphore(int count, int max_count);
+	
+	// Increments count and notifies waiting threads
+	// Returns current value of m_count
+	int Post();
+
+	// Blocks thread until m_count is greater than 0, decrements count when access is granted
+	// Returns current value of m_count
+	int Wait();
+
+	// Returns the current value of m_count
+	int GetValue();
+};
+
---
+++ Semaphore/src/UTIL_Semaphore.cpp
@@ -0,0 +1,52 @@
+#include "UTIL_Semaphore.h"
+
+UTIL_Semaphore::UTIL_Semaphore(){
+	this->m_count = MIN_COUNT;
+	this->m_max_count = MAX_COUNT_INFINITE;
+}
+
+UTIL_Semaphore::UTIL_Semaphore(int count){
+	// Validate provided count
+	if (count < MIN_COUNT)
+		count = MIN_COUNT;
+	
+	this->m_count = count;
+	this->m_max_count = MAX_COUNT_INFINITE;
+}
+
+UTIL_Semaphore::UTIL_Semaphore(int count, int max_count){
+	// Validate provided count
+	if (count < MIN_COUNT)
+		count = MIN_COUNT;
+
+	// Validate provided max count
+	if (max_count < MIN_COUNT)
+		max_count = MAX_COUNT_INFINITE;
+	if (max_count < count)
+		max_count = count;
+
+
+	this->m_count = count;
+	this->m_max_count = max_count;
+}
+
+int UTIL_Semaphore::Post(){
+	std::unique_lock<std::mutex> lock(this->m_mutex);
+	if (this->m_max_count == MAX_COUNT_INFINITE || this->m_count < this->m_max_count) {
+		this->m_count++;
+		this->m_cv.notify_one();
+	}
+	return this->m_count;
+}
+
+int UTIL_Semaphore::Wait(){
+	std::unique_lock<std::mutex> lock(this->m_mutex);
+	while (this->m_count == 0)
+		this->m_cv.wait(lock);
+	this->m_count--;
+	return this->m_count;
+}
+
+int UTIL_Semaphore::GetValue(){
+	return this->m_count;
+}
